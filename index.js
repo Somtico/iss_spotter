1262482e56dea68c1f7c8e1d6f273048844cbef6
Somto Ufondu <somtoufondu@gmail.com>
2024-04-26 14:53:12 -0600

    Write code to test the fetchCoordsByIp function.

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const { fetchMyIP } = require('./iss');
+// const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
 
-  console.log('It worked! Returned IP:' , ip);
-});
+//   console.log('It worked! Returned IP:' , ip);
+// });
 
+// fetchCoordsByIP("71.17.145.130", (error, data) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned Geolocation:' , data);
+// });
